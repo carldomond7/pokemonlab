f2d0ea55a56cf979e787482d21191a287542036d
carldomond7 <124755395+carldomond7@users.noreply.github.com>
2023-09-21 13:02:51 -0700

    File contents will now be the pokemon ability and the description of the effect of that ability

--- poke.ps1
+++ poke.ps1
@@ -3,11 +3,37 @@
 $apiURL = "https://pokeapi.co/api/v2/pokemon-species/infernape"
 
 #Retrieve Pokemon info
-$infernape_info = Invoke-RestMethod -Uri $apiURL -Method Get
+$infernape_info = Invoke-RestMethod -Uri $apiURL 
+
+$moveset = @()
+$description = @()
+
+#Parse through pokemon info and append the abilities NAME to the moveset array
+foreach($move in $infernape_info.abilities) {
+    $moveset += $move.ability.name
+}
+
+foreach($des in $infernape_info.abilities)
+{
+    $api = $des.ability.url
+    $Moveinfo = Invoke-RestMethod -Uri $api
+    $parsedinfo = $Moveinfo.effect_entries[1].effect
+    $description += $parsedinfo
+}
+
+$combinedarray = @()
+
+for($i = 0; $i -lt $moveset.Count; $i++)
+{
+    $combinedarray += $moveset[$i]
+    $combinedarray += "`n"
+    $combinedarray += $description[$i]
+    $combiendarray += "`n"
+}
 
 #Create unique filename
 $uniqueFilename = "infernape.txt" + (Get-Date -Format "yyyyMMddHHmmss") 
-$infernape_info | Set-Content -Path $uniqueFilename
+$combinedarray | Set-Content -Path $uniqueFilename
 
 
 #Commit and push changes to Github
