72ab5afa70e64ef698fe6bdaa2079cd97d803b84
carldomond7 <124755395+carldomond7@users.noreply.github.com>
2023-09-21 14:19:49 -0700

    Update poke.ps1

--- poke.ps1
+++ poke.ps1
@@ -1,41 +1,51 @@
 ##########Pokemon API call#############
-
-$apiURL = "https://pokeapi.co/api/v2/pokemon-species/infernape"
-
 #Retrieve Pokemon info
-$infernape_info = Invoke-RestMethod -Uri $apiURL 
+#Parse through pokemon info and append the abilities NAME to the moveset array
+
+$inf = Invoke-RestMethod "https://pokeapi.co/api/v2/pokemon/infernape" 
 
 $moveset = @()
 $description = @()
-
-#Parse through pokemon info and append the abilities NAME to the moveset array
-foreach($move in $infernape_info.abilities) {
-    $moveset += $move.ability.name
+foreach ($move in $inf.abilities) {
+    $moveset += $move.ability.name 
 }
 
-foreach($des in $infernape_info.abilities)
+foreach ($des in $inf.abilities)
 {
-    $api = $des.ability.url
-    $Moveinfo = Invoke-RestMethod -Uri $api
-    $parsedinfo = $Moveinfo.effect_entries[1].effect
-    $description += $parsedinfo
+    $api = $des.ability.url 
+    $response = Invoke-RestMethod -Uri $api
+    $engresponse = $response.effect_entries[1].effect 
+    $description += $engresponse
 }
 
+#Write-Host "$($moveset)"
+#Write-Host "$($description)"
 $combinedarray = @()
 
+#For loop to create finalized array
 for($i = 0; $i -lt $moveset.Count; $i++)
 {
+    #Adding the particular move at this index to the array
     $combinedarray += $moveset[$i]
+    #Creating a new line after that move
     $combinedarray += "`n"
+    #Adding corresponding description to that move 
     $combinedarray += $description[$i]
     $combinedarray += "`n"
 }
+
 
 #Create unique filename
 $timestamp = Get-Date -Format "yyyyMMddHHmmss"
 $uniqueFilename = "infernape_$timestamp.txt"
 $combinedarray | Set-Content -Path $uniqueFilename
 
+if (Test-Path -Path $uniqueFilename) {
+    Write-Host "File created successfully."
+} else {
+    Write-Host "File creation failed or the file does not exist."
+}
 
-#Commit and push changes to Github
 git add $uniqueFilename
+
+
